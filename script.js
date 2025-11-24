0ddcd67b52e1a3913fcb7ff688c777850ad0ac8a
Sai <akulasairam6303@gmail.com>
2025-11-24 14:22:08 +0530

    update

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ fetch("https://fakestoreapi.com/products/")
     ram.innerHTML = `<img src="${c.image}" />
                         <h3>${c.title.slice(0,50)}...</h3>
                         <p>${c.description.slice(0,50)}...</p>
-                        <h5>price : ${c.price + `$`}</h5>`;
+                        <h5>price : ${`$` + c.price}</h5>`;
 
     app.appendChild(ram);
 
